cf657b60bbb0a5656e48b563b8a09f48c1a05b7d
VirajRanpara <virajranpara111@gmail.com>
2022-06-22 10:29:18 +0530

    Hosted

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 var router = require('./route')
 
+mongoose.connect("mongodb+srv://viraj123:V3915791@cluster0.w1sz0.mongodb.net/Student?retryWrites=true&w=majority").then(()=>{
+    console.log("DB Connected");
+
+    app = express()
+    app.use(bodyParser.urlencoded({extended:false}))
+    app.use(express.json())
+    app.use('/api',router)
+
+    app.listen(process.env.PORT,()=>{
+        console.log("Server Started.");
+    })
+}).catch((err)=>{
+    console.log(err);
+})
+
+/*
 var app = express()
 app.use('/api',router)
 app.listen(process.env.PORT,()=>{
     console.log("Server Started");
-})
+}) */
